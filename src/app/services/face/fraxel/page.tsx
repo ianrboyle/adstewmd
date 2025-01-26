2d06c1c9a456785d93277d23aa3fdccec8dd60f2
Ian Boyle <irboyle21@gmail.com>
2025-01-26 10:29:30 -0700

    add microneedling page

--- src/app/services/face/fraxel/page.tsx
+++ src/app/services/face/fraxel/page.tsx
@@ -1,5 +1,11 @@
 import Fraxel from "../../../../components/featured-services/inidividual-pages/face/Fraxel";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Fraxel Laser Treatment Denver CO",
+  description:
+    "Fraxel Laser Treatment offered in Cherry Creek, Denver, CO and serving patients from the Denver Metropolitan Area",
+};
 export default function FraxelPage() {
   return <Fraxel />;
 }
---
+++ src/constants/featured-services/face/microneedling.ts
@@ -0,0 +1,54 @@
+export const microneedlingMainTitle = "Microneedling";
+
+export const microneedlingMainText =
+  "If lines, wrinkles, scarring, and other imperfections negatively affect your self-confidence, a simple aesthetic solution is within reach at Aesthetic Surgery and Dermatology of Cherry Creek in Denver, Colorado. Board-certified dermatologist Adrienne Stewart, MD, and medical aestheticians Jodi Payne and Willa Roberts offer microneedling treatments that stimulate new cell production to achieve younger-looking, healthier, beautiful skin without downtime. Call the office to schedule a cosmetic consultation, or use the online booking tool today.";
+
+export const whatIsMicroneedlingTitle = "What is microneedling?";
+
+export const whatIsMicroneedlingBody = [
+  "Microneedling is a noninvasive cosmetic treatment that minimizes unwelcome signs of aging without surgery or other invasive procedures.",
+
+  "The procedure uses a handheld device containing tiny needles that make small tears in treated skin without leaving scars. These micropunctures stimulate new cell production, allowing treated skin to repair itself naturally while stimulating new collagen growth, allowing treated skin to repair itself naturally while stimulating new collagen gowth.",
+];
+
+export const microneedlingBenefitsTitle = "What are the benefits of microneedling?";
+export const microneedlingBenefitsMainBody = [
+  "The advantages of microneedling at Aesthetic Surgery and Dermatology of Cherry Creek include:",
+];
+
+export const microneedlingBenefitsListItems = [
+  "Fewer lines and wrinkles",
+  "Younger-looking skin",
+  "Smoother, tighter skin",
+  "Increased collagen and elastin production",
+  "Reduced acne scarring",
+  "Reductions in pore size",
+  "Diminished stretch marks",
+];
+
+export const microneedlingBenefitsSubtext =
+  "Microneedling is safe, approved by the Food and Drug Administration (FDA), and highly effective as an anti-aging treatment. Microneedling is safe for people with hyperpigmentation challenges.";
+
+export const isMicroneedlingRightForMeTitle = " Is microneedling right for me?";
+export const isMicroneedlingRightForMeBody = [
+  "To determine if you’re a candidate for microneedling, an Aesthetic Surgery and Dermatology of Cherry Creek provider discusses your beauty goals, medical history, and treatment preferences. They tailor a personalized treatment that ensures you achieve the desired results. Your specialist may combine multiple aesthetic treatments to perfect your outcome.",
+];
+
+export const afterMicroneedlingTitle = "What happens after microneedling?";
+export const afterMicroneedlingMainBody = [
+  "After you undergo microneedling at Aesthetic Surgery and Dermatology of Cherry Creek, it’s important to keep treated skin clean and protected from the sun. Temporary side effects may include swelling, redness, and tenderness.",
+  "Over time, as treated skin regenerates cells, you can expect revitalized skin with a healthy glow. Repeat microneedling treatments offer exceptional, long-lasting results. Your Aesthetic Surgery and Dermatology of Cherry Creek specialist lets you know when to schedule your next session.",
+  "We offer these types of microneedling:",
+];
+
+export const afterMicroneedlingListItems = ["Scarlet", "Venus Viva", "Evopen"];
+
+export const afterMicroneedlingSubtext =
+  "Call the Aesthetic Surgery and Dermatology of Cherry Creek office to learn more about microneedling, or use the online booking tab today.";
+
+export const whatCanIExpectMicroneedlingTitle = "What should I expect during microneedling?";
+export const whatCanIExpectMicroneedlingBody = [
+  "During microneedling procedures at Aesthetic Surgery and Dermatology of Cherry Creek, you rest comfortably in a relaxing room. Your specialist cleans and numbs the treatment area to ensure maximum comfort.",
+
+  "They use a handheld device and tiny needles to make small skin punctures. Your provider might also apply a serum to treated skin to enhance healing. The treatment ranges from 30-60 minutes",
+];
